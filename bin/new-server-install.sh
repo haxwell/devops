e23697b188e6b0308c826d0e3704b14a56cdf668
Johnathan James <haxwell@gmail.com>
2019-09-07 09:32:47 -0600

    Fix mistaken path name, haxwell-devops/bin/apps to haxwell-devops/apps

--- bin/new-server-install.sh
+++ bin/new-server-install.sh
@@ -92,7 +92,7 @@ chown quizki /home/quizki/apps/node -R
 chgrp quizki /home/quizki/apps/node -R
 
 cd node
-scp $HAX_CONFIG_SERVER_USER_NAME@$HAX_CONFIG_SERVER_IP:/home/$HAX_CONFIG_SERVER_USER_NAME/haxwell-devops/bin/apps/node-v10.16.3-linux-x64.tar.xz node-v10.16.3-linux-x64.tar.xz
+scp $HAX_CONFIG_SERVER_USER_NAME@$HAX_CONFIG_SERVER_IP:/home/$HAX_CONFIG_SERVER_USER_NAME/haxwell-devops/apps/node-v10.16.3-linux-x64.tar.xz node-v10.16.3-linux-x64.tar.xz
 tar -xvf ./node-v10.16.3-linux-x64.tar.xz
 ln -s node-v10.16.3-linux-x64 current
 }
@@ -112,14 +112,14 @@ chown quizki /home/quizki/apps/java -R
 chgrp quizki /home/quizki/apps/java -R
 
     cd java
-scp $HAX_CONFIG_SERVER_USER_NAME@$HAX_CONFIG_SERVER_IP:/home/$HAX_CONFIG_SERVER_USER_NAME/haxwell-devops/bin/apps/jdk-8u221-linux-x64.tar.gz ./jdk8221.tar.gz
+scp $HAX_CONFIG_SERVER_USER_NAME@$HAX_CONFIG_SERVER_IP:/home/$HAX_CONFIG_SERVER_USER_NAME/haxwell-devops/apps/jdk-8u221-linux-x64.tar.gz ./jdk8221.tar.gz
 tar -xvf ./jdk8221.tar.gz
 ln -s jdk1.8.0_221 current
 
 chown quizki /home/quizki/apps/java/current -R
 chgrp quizki /home/quizki/apps/java/current -R
 
-scp $HAX_CONFIG_SERVER_USER_NAME@$HAX_CONFIG_SERVER_IP:/home/$HAX_CONFIG_SERVER_USER_NAME/haxwell-devops/bin/apps/mysql-connector-java-8.0.17.jar /home/quizki/apps/java/current/lib/.
+scp $HAX_CONFIG_SERVER_USER_NAME@$HAX_CONFIG_SERVER_IP:/home/$HAX_CONFIG_SERVER_USER_NAME/haxwell-devops/apps/mysql-connector-java-8.0.17.jar /home/quizki/apps/java/current/lib/.
 
 chown quizki /home/quizki/apps/java/current -R
 chgrp quizki /home/quizki/apps/java/current -R
